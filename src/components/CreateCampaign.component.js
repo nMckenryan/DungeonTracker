da068ca684af603ad802b391fc98985fce48bda5
Nigel Mckenzie-Ryan <n.mckenryan@gmail.com>
2021-03-09 13:30:07 +1100

    Replacing Procfile

--- src/components/CreateCampaign.component.js
+++ src/components/CreateCampaign.component.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
 
 export default class CreateCampaign extends Component {
 
